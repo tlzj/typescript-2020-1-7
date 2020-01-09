5bb43906b2050903b1681f3f37d52aaf26bec90d
tlzj <quesanbo@netposa.com>
2020-01-09 17:32:56 +0800

    fix

--- demo20200109/2.进阶.ts
+++ demo20200109/2.进阶.ts
@@ -124,3 +124,79 @@
 // c(10);
 // c.reset();
 // c.interval = 5.0;
+
+// 7.泛型 (在定义函数、接口或类的时候，不预先指定具体的类型，而在使用的时候再指定类型的一种特性。)
+// 这个例子不能指定返回的[]里面数据的类型,需要使用泛型来限定
+// function createArray(length: number, value: any): Array<any> {
+//   let result = [];
+//   for(let i = 0; i < length; i++){
+//     result[i] = value;
+//   }
+//   return result;
+// }
+// createArray(3, 'x');
+// 以下改为泛型
+// function createArray<T>(length: number, value: T): Array<T> {
+//   let result: T[] = [];
+//   for(let i = 0; i < length; i++){
+//     result[i] = value;
+//   }
+//   return result;
+// }
+// createArray<string>(3, '2341');
+// createArray(3, 23452); // 不指定类型时，类型推论会自己指定类型
+
+// 定义多个类型参数
+// function swap<T, U>(tuple: [T, U]): [U, T]{
+//   return [tuple[1], tuple[0]];
+// }
+
+// 泛型约束(extends)
+// interface Lengthwise {
+//   length: number;
+// }
+// function loggingIdentity<T extends Lengthwise>(arg: T): T {
+//   console.log(arg.length);
+//   return arg;
+// }
+// loggingIdentity({ name: 'zs', length: 213 })
+
+// function copyFields<T extends U, U>(target: T, source: U): T {
+//   for(let id in source){
+//     target[id] = (<T>source)[id];
+//   }
+//   return target;
+// }
+
+// interface CreateArrayFunc<T> {
+//   (length: number, value: T): Array<T>;
+// }
+
+// let createArray: CreateArrayFunc<string | number>;
+// createArray = function<T>(length: number, value: T): Array<T> {
+//   let result: T[] = [];
+//   for (let i = 0; i < length; i++) {
+//       result[i] = value;
+//   }
+//   return result;
+// }
+
+// createArray(3, 'x'); // ['x', 'x', 'x']
+
+// 8.声明合并
+interface Alarm {
+  price: number;
+  alert(s: string): string;
+}
+interface Alarm {
+  weight: number;
+  alert(s: string, n: number): string;
+}
+
+let fn: Alarm = {
+  price: 23,
+  weight: 90,
+  alert: function(s: string): string{
+    return s;
+  }
+}
